7455b41f99c9070dd6b16124b5c316a2fa00737c
Matheus Mesquita <matheusmeskitah@gmail.com>
2024-03-24 16:22:38 -0300

    Added DTOs

---
+++ tracker/tracker_data/src/main/java/com/meskitah/tracker_data/remote/dto/Product.kt
@@ -0,0 +1,11 @@
+package com.meskitah.tracker_data.remote.dto
+
+import com.squareup.moshi.Json
+
+data class Product(
+    @Json(name = "image_front_thumb_url")
+    val imageFrontThumbUrl: String?,
+    @Json(name = "product_name")
+    val productName: String?,
+    val nutriments: Nutriments
+)
