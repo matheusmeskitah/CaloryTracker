9ba8dfb0b7f5a3d4c3d45743115b75bad7497c82
Matheus Mesquita <matheusmeskitah@gmail.com>
2024-03-24 22:37:41 -0300

    Added meal items

--- tracker/tracker_presentation/src/main/java/com/meskitah/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/meskitah/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -13,6 +14,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.meskitah.core.util.UiEvent
 import com.meskitah.core_ui.LocalSpacing
 import com.meskitah.tracker_presentation.tracker_overview.components.DaySelector
+import com.meskitah.tracker_presentation.tracker_overview.components.ExpandableMeal
 import com.meskitah.tracker_presentation.tracker_overview.components.NutrientsHeader
 
 @Composable
@@ -48,6 +50,18 @@ fun TrackerOverviewScreen(
             )
 
             Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        }
+        items(state.meals) { meal ->
+            ExpandableMeal(
+                meal = meal,
+                onToggleClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnToggleMealClick(meal))
+                },
+                content = {
+
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
